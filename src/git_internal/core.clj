d85bea329a17839d3f75127071352361822976c6
Daehyun Kim <hatemogi@gmail.com>
2016-07-18 20:52:06 +0900

    add a function to write tree-object

--- src/git_internal/core.clj
+++ src/git_internal/core.clj
@@ -5,18 +5,27 @@
            java.security.MessageDigest
            [java.util.zip DeflaterOutputStream InflaterInputStream]))
 
+(defprotocol ObjectId
+  (raw-bytes [this])
+  (hex-str [this]))
+
+(extend-protocol ObjectId
+  (class (byte-array 0))
+  (raw-bytes [this] this)
+  (hex-str [this] (format "%040x" (BigInteger. 1 this))))
+
 (defn- sha1update
   ([bytes] (sha1update (MessageDigest/getInstance "SHA-1") bytes))
   ([digest bytes] (.update digest bytes) digest))
 
 (defn- sha1final [digest]
-  (format "%040x" (BigInteger. 1 (.digest digest))))
+  (hex-str (.digest digest)))
 
 (def sha1hex (comp sha1final sha1update))
 
 (defn blob-digest [bytes]
-  (let [header (.getBytes (str "blob " (alength bytes) "\0"))]
-    (-> (sha1update header)
+  (let [header (str "blob " (alength bytes) "\0")]
+    (-> (sha1update (.getBytes header))
         (sha1update bytes)
         sha1final)))
 
@@ -37,3 +46,15 @@
   (stream->bytes [out]
     (with-open [in (InflaterInputStream. (ByteArrayInputStream. bytes))]
       (io/copy in out))))
+
+(defrecord TreeEntry [mode type oid filename])
+
+(defn tree-entries->bytes [entries]
+  (stream->bytes [out]
+    (doseq [{:keys [mode type oid filename]} entries]
+      (doto out
+        (.write (.getBytes (name mode)))
+        (.write 32)
+        (.write (.getBytes filename))
+        (.write 0)
+        (.write (raw-bytes oid))))))
