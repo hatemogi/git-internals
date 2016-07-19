a50da21cfbf4186f768534d90d945046e63ddf43
Daehyun Kim <hatemogi@gmail.com>
2016-07-19 13:36:35 +0900

    handling raw bytes is a pain. ztellman/gloss looks promising.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/hatemogi/git-internal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [gloss "0.2.5"]]
   :main ^:skip-aot git-internal.core
   :target-path "target/%s"
   :plugins [[lein-codox "0.9.5"]]
