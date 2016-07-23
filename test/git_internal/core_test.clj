cac64c8464c98106ea2ef0b1603d4e01e47357a4
Daehyun Kim <hatemogi@gmail.com>
2016-07-23 12:49:03 +0900

    give it a try to encode tree-object using ztellman/gloss.

--- test/git_internal/core_test.clj
+++ test/git_internal/core_test.clj
@@ -1,9 +1,9 @@
 (ns git-internal.core-test
   (:require [clojure.test :refer :all]
             [clojure.test.check
-                [clojure-test :refer [defspec]]
-                [generators :as gen]
-                [properties :as prop]]
+             [clojure-test :refer [defspec]]
+             [generators :as gen]
+             [properties :as prop]]
             [git-internal.core :refer :all]))
 
 (defspec sha1hex-len-spec
@@ -12,7 +12,7 @@
 
 (deftest blob-digest-test
   (testing "sample"
-    (are [obj-id str] (= obj-id (blob-digest (.getBytes str)))
+    (are [obj-id str] (= obj-id (-> str .getBytes blob-digest hex-str))
       "d670460b4b4aece5915caf5c68d12f560a9fe3e4" "test content\n"
       "bd9dbf5aae1a3862dd1526723246b20206e5fc37" "what is up, doc?")))
 
@@ -21,5 +21,10 @@
                 (= (seq v) (seq (inflate (deflate v))))))
 
 (defspec tree->bytes-spec
-  (prop/for-all [entries (gen/vector (gen/tuple gen/bytes))]
-                true))
+  (prop/for-all [tree (gen/not-empty
+                       (gen/vector (gen/hash-map
+                                    :mode (gen/elements ["100644" "100755" "120000" "040000"])
+                                    :filename (gen/not-empty gen/string-alphanumeric)
+                                    :oid (gen/fmap byte-array (gen/vector gen/byte 20)))))]
+                (= (seq (-tree->bytes tree))
+                   (seq (tree->bytes tree)))))
